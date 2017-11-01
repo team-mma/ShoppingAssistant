6fef9fe677532b816b831b4b1b48008987cc12ac
Gubbala Madhuri <gubbalamadhuri1991@gmail.com>
2017-10-31 19:48:13 -0700

    Adding jQuery dependency and functions to remove items from list

--- static/main.js
+++ static/main.js
@@ -7,3 +7,9 @@ function removeItem(){
     var ul = document.getElementById("productlist");
     //TODO: remove node from list
 }
+
+//Removes the item for now
+$("ul").on("click", "i", function(e) {
+    e.preventDefault();
+    $(this).parent().remove();
+});
