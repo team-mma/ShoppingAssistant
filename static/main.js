a189f70854a3e7fffca70602df4d9832a530361f
Gubbala Madhuri <gubbalamadhuri1991@gmail.com>
2017-10-31 19:29:12 -0700

    Create event handlers for adding and removing items from the list

---
+++ static/main.js
@@ -0,0 +1,9 @@
+function addItem(){
+    var ul = document.getElementById("productlist");
+    //TODO: update text on list item
+}
+
+function removeItem(){
+    var ul = document.getElementById("productlist");
+    //TODO: remove node from list
+}
