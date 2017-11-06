99f50ed71717f97304e0b3a09ef579fd0a2b88c4
Maria Kalyuzhny <makalyuz@ucsd.edu>
2017-11-06 00:56:49 -0800

    Dynamically adding elements using Handlebars

--- static/main.js
+++ static/main.js
@@ -1,14 +1,35 @@
+//compile the template
+var source = $("#list-item-template").html();
+var template = Handlebars.compile(source);
+
+var parentDiv = $("#templatedLists");
+
+//start with a simple template
+/**var html = template(simpleData); //fill out simpleData
+console.log(html);
+parentDiv.append(html);**/
+
+//iterate through the complexData list and append:
+for (var i = 0; i < shoppingHistoryProducts.length; i++) {
+  //var curData = shoppingHistoryProducts[i];
+  currentProducts.push(shoppingHistoryProducts[i]);
+  currentProducts[i].id = "elem"+i.toString();
+  var curData = currentProducts[i];
+  var curHtml = template(curData);
+  parentDiv.append(curHtml);
+}
+
 function addItem(){
     var ul = document.getElementById("productlist");
     //TODO: update text on list item
 }
 
 function removeItem(){
-    var ul = document.getElementById("productlist");
+    var elem = document.getElementById("productlist");
     //TODO: remove node from list
 }
 
-//Removes the item for now
+//Removes or adds the item for now
 $("ul").on("click", "i", function(e) {
     e.preventDefault();
     if(this.className==="arrow left") {
---
+++ static/storage.js
@@ -0,0 +1,14 @@
+var shoppingHistoryProducts = [
+  {'productTitle': 'Milk', 'productPic': 'milk.svg', 'productQuantity': 2,
+  'productAmount': '2','productUnits': 'qts', 'productCost': '$5.00', 'id': 1},
+  {'productTitle': 'Danone Cheese', 'productPic': 'milk.svg', 'productQuantity': 1,
+  'productAmount': '12','productUnits': 'oz', 'productCost': '$7.00', 'id': 1},
+  {'productTitle': 'Lindt Assorted Truffles', 'productPic': 'milk.svg', 'productQuantity': 1,
+  'productAmount': '16','productUnits': 'oz', 'productCost': '$7.50', 'id': 1},
+  {'productTitle': 'Hollandia Butter', 'productPic': 'milk.svg', 'productQuantity': 3,
+  'productAmount': '10','productUnits': 'oz', 'productCost': '$3.60', 'id': 1},
+  {'productTitle': 'Honey Bunches of Oats Cereal', 'productPic': 'milk.svg', 'productQuantity': 1,
+  'productAmount': '14.5','productUnits': 'oz', 'productCost': '$6.99', 'id': 1},
+];
+
+var currentProducts = [];
