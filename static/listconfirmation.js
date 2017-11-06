0b6f983b4053a3aced53914c8b2baea78cc31ab2
Abhilash Srivastava <abhilash.shakti@gmail.com>
2017-11-06 14:31:05 -0800

    Updating local storage on left and right swipe

--- static/listconfirmation.js
+++ static/listconfirmation.js
@@ -2,13 +2,16 @@ var source = $("#conf-item-template").html();
 var template = Handlebars.compile(source);
 var parentDiv = $("#templatedConfs");
 
-var savedShoppingProductsList = JSON.parse(localStorage.getItem('shoppingHistoryProducts'));
+$(document).ready(function(){
+    var savedShoppingProductsList = JSON.parse(localStorage.getItem('shoppingHistoryProducts'));
 
-for (var i = 0; i < savedShoppingProductsList.length; i++) {
-  //var curData = shoppingHistoryProducts[i];
-  //currentProducts.push(shoppingHistoryProducts[i]);
-  //currentProducts[i].id = "elem"+i.toString();
-  var curData = savedShoppingProductsList[i];
-  var curHtml = template(curData);
-  parentDiv.append(curHtml);
-}
+    for (var i = 0; i < savedShoppingProductsList.length; i++) {
+      //var curData = shoppingHistoryProducts[i];
+      //currentProducts.push(shoppingHistoryProducts[i]);
+      //currentProducts[i].id = "elem"+i.toString();
+      var curData = savedShoppingProductsList[i];
+      var curHtml = template(curData);
+      parentDiv.append(curHtml);
+    }
+});
+
--- static/main.js
+++ static/main.js
@@ -4,37 +4,44 @@ var template = Handlebars.compile(source);
 
 var parentDiv = $("#templatedLists");
 
-//start with a simple template
-/**var html = template(simpleData); //fill out simpleData
-console.log(html);
-parentDiv.append(html);**/
-
 var savedShoppingProductsList = JSON.parse(localStorage.getItem('shoppingHistoryProducts'));
 for (var i = 0; i < savedShoppingProductsList.length; i++) {
   //var curData = shoppingHistoryProducts[i];
   currentProducts.push(savedShoppingProductsList[i]);
-  currentProducts[i].id = "elem"+i.toString();
+  currentProducts[i].id = i.toString();
   var curData = currentProducts[i];
   var curHtml = template(curData);
   parentDiv.append(curHtml);
 }
 
-function addItem(){
-    var ul = document.getElementById("productlist");
-    //TODO: update text on list item
+function addItem(i){
+    console.log('add item:',i);
+    var savedShoppingProductsList = JSON.parse(localStorage.getItem('shoppingHistoryProducts'));
+    var prodQuantity = savedShoppingProductsList[i].productQuantity;
+    alert("Quantity updated: "+prodQuantity);
+    savedShoppingProductsList[i].productQuantity = savedShoppingProductsList[i].productQuantity + 1;
+    localStorage.setItem('shoppingHistoryProducts',JSON.stringify(savedShoppingProductsList));
+    
+    // TODO: The localStorage is updated. The page should refresh to show the updated changes
 }
 
-function removeItem(){
-    var elem = document.getElementById("productlist");
-    //TODO: remove node from list
+function removeItem(i){
+    console.log('remove item:',i);
+    var savedShoppingProductsList = JSON.parse(localStorage.getItem('shoppingHistoryProducts'));
+    var prodTitle = savedShoppingProductsList[i].productTitle;
+    alert("Deleting "+prodTitle+" ...");
+    delete savedShoppingProductsList[i];
+    savedShoppingProductsList.splice(i,1);
+    localStorage.setItem('shoppingHistoryProducts',JSON.stringify(savedShoppingProductsList));
 }
 
 //Removes or adds the item for now
 $("ul").on("click", "i", function(e) {
+    console.log('click');
     e.preventDefault();
     if(this.className==="arrow left") {
         $(this).parent().remove();
     } else if(this.className==="arrow right") {
-        alert("Quantity update noted!");
+//        alert("Quantity update noted!");
     }
 });
