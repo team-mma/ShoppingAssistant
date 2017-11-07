6aee2484ba3c3a8fab07b830ff3f5d83d6a04871
Abhilash Srivastava <abhilash.shakti@gmail.com>
2017-11-06 17:28:23 -0800

    Bugs fixed in main page

--- static/main.js
+++ static/main.js
@@ -6,23 +6,32 @@ var parentDiv = $("#templatedLists");
 
 var savedShoppingProductsList = JSON.parse(localStorage.getItem('shoppingHistoryProducts'));
 for (var i = 0; i < savedShoppingProductsList.length; i++) {
-  //var curData = shoppingHistoryProducts[i];
   currentProducts.push(savedShoppingProductsList[i]);
-//  currentProducts[i].id = i.toString();
   var curData = currentProducts[i];
   var curHtml = template(curData);
   parentDiv.append(curHtml);
 }
 
-function addItem(i){
-    console.log('add item:',i);
+function addItem(id){
+    console.log('add id:',id);
     var savedShoppingProductsList = JSON.parse(localStorage.getItem('shoppingHistoryProducts'));
-    var prodQuantity = savedShoppingProductsList[i].productQuantity;
-    alert("Quantity updated: "+prodQuantity);
-    savedShoppingProductsList[i].productQuantity = savedShoppingProductsList[i].productQuantity + 1;
+    increaseItemCount(id, savedShoppingProductsList);
     localStorage.setItem('shoppingHistoryProducts',JSON.stringify(savedShoppingProductsList));
     
     // TODO: The localStorage is updated. The page should refresh to show the updated changes
+}
+
+function increaseItemCount(id, savedShoppingProductsList) {
+    for(var i = 0; i < savedShoppingProductsList.length; i++) {
+        var prod = savedShoppingProductsList[i];
+        if(prod.id == id) {
+            var prodTitle = savedShoppingProductsList[i].productTitle;
+            alert("Quantity updated: "+prodTitle);
+            savedShoppingProductsList[i].productQuantity = 
+                savedShoppingProductsList[i].productQuantity + 1;
+            return;
+        }
+    }
 }
 
 function removeItem(id){
