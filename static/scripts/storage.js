7ee3c705b5f4e609195eb34632151da9a6548e3a
Maria Kalyuzhny <makalyuz@ucsd.edu>
2017-11-09 12:59:11 -0800

    initiate local storage in index.js

--- static/scripts/storage.js
+++ static/scripts/storage.js
@@ -21,4 +21,27 @@ var shoppingHistoryProducts = [
   'productAmount': '10','productUnits': 'qts', 'productCost': '$6.99', 'id': 9},
 ];
 
-var currentProducts = [];
+var storeProducts = [
+  {'productTitle': 'Milk', 'productPic': '../images/milk.svg', 'productQuantity': 2,
+  'productAmount': '2','productUnits': 'qts', 'productCost': '$5.00', 'id': 0},
+  {'productTitle': 'Danone Cheese', 'productPic': '../images/milk.svg', 'productQuantity': 1,
+  'productAmount': '12','productUnits': 'oz', 'productCost': '$7.00', 'id': 1},
+  {'productTitle': 'Lindt Assorted Truffles', 'productPic': '../images/milk.svg', 'productQuantity': 1,
+  'productAmount': '16','productUnits': 'oz', 'productCost': '$7.50', 'id': 2},
+  {'productTitle': 'Hollandia Butter', 'productPic': '../images/milk.svg', 'productQuantity': 3,
+  'productAmount': '10','productUnits': 'oz', 'productCost': '$3.60', 'id': 3},
+  {'productTitle': 'Honey Bunches of Oats Cereal', 'productPic': '../images/milk.svg', 'productQuantity': 1,
+  'productAmount': '14.5','productUnits': 'oz', 'productCost': '$6.99', 'id': 4},
+    {'productTitle': 'Chocolate 1', 'productPic': '../images/milk.svg', 'productQuantity': 1,
+  'productAmount': '10','productUnits': 'qts', 'productCost': '$6.99', 'id': 5},
+    {'productTitle': 'Chocolate 2', 'productPic': '../images/milk.svg', 'productQuantity': 1,
+  'productAmount': '10','productUnits': 'qts', 'productCost': '$6.99', 'id': 6},
+    {'productTitle': 'Chocolate 3', 'productPic': '../images/milk.svg', 'productQuantity': 1,
+  'productAmount': '10','productUnits': 'qts', 'productCost': '$6.99', 'id': 7},
+    {'productTitle': 'Chocolate 4', 'productPic': '../images/milk.svg', 'productQuantity': 1,
+  'productAmount': '10','productUnits': 'qts', 'productCost': '$6.99', 'id': 8},
+    {'productTitle': 'Chocolate 5', 'productPic': '../images/milk.svg', 'productQuantity': 1,
+  'productAmount': '10','productUnits': 'qts', 'productCost': '$6.99', 'id': 9},
+];
+
+//var currentProducts = [];
