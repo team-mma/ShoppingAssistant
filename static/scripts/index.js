79bfe14565daab07e8ea5c18000b612cb5ac50c4
Maria Kalyuzhny <makalyuz@ucsd.edu>
2017-11-14 18:38:29 -0800

    Wizard of OZ creating two users

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -3,6 +3,34 @@ $(document).ready(function () {
 
     localStorage.setItem('storeProducts',JSON.stringify(storeProducts));
     //TODO later. add condition so that shopping history products will be based on past history
-    localStorage.setItem('shoppingHistoryProducts',JSON.stringify(shoppingHistoryProducts));
-    localStorage.setItem('currentProducts',JSON.stringify(shoppingHistoryProducts));
+
 });
+
+function login() {
+    var x = document.getElementById("frm1");
+    var name = "";
+    var password = "";
+    name += x.elements[0].value;
+    password += x.elements[1].value;
+    console.log(name);
+    console.log(password);
+    if (("Larry").valueOf() == name.valueOf() && ("12345").valueOf() == password.valueOf()) {
+      localStorage.setItem('user',name);
+      localStorage.setItem('password',password);
+      localStorage.setItem('shoppingHistoryProducts',JSON.stringify(shoppingHistoryProducts[0]));
+      localStorage.setItem('currentProducts',JSON.stringify(shoppingHistoryProducts[0]));
+      window.location.pathname = '/main.html'
+    }
+    else if (("Lucy").valueOf() == name.valueOf() && ("6789").valueOf() == password.valueOf()) {
+      localStorage.setItem('user',name);
+      localStorage.setItem('password',password);
+      localStorage.setItem('shoppingHistoryProducts',JSON.stringify(shoppingHistoryProducts[1]));
+      localStorage.setItem('currentProducts',JSON.stringify(shoppingHistoryProducts[1]));
+      window.location.pathname = '/main.html';
+    }
+    else {
+      alert("Invalid login: Try Larry 12345 or Lucy 6789");
+    }
+    return;
+    //document.getElementById("demo").innerHTML = password;
+}
--- static/scripts/storage.js
+++ static/scripts/storage.js
@@ -1,28 +1,55 @@
-// This is the list of products from the user's shopping history. 
+// This is the list of products from the user's shopping history.
 // This is static for now(does not change with user adding or deleting the products)
 // Can be personalized for each user
-let shoppingHistoryProducts = [
-    {
-        'productTitle': 'Milk', 'productPic': '../images/milk.svg', 'productQuantity': 2,
-        'productAmount': '2', 'productUnits': 'qts', 'productCost': '$5.00', 'id': 0
-    },
-    {
-        'productTitle': 'Danone Cheese', 'productPic': '../images/milk.svg', 'productQuantity': 1,
-        'productAmount': '12', 'productUnits': 'oz', 'productCost': '$7.00', 'id': 1
-    },
-    {
-        'productTitle': 'Lindt Assorted Truffles', 'productPic': '../images/milk.svg', 'productQuantity': 1,
-        'productAmount': '16', 'productUnits': 'oz', 'productCost': '$7.50', 'id': 2
-    },
-    {
-        'productTitle': 'Hollandia Butter', 'productPic': '../images/milk.svg', 'productQuantity': 3,
-        'productAmount': '10', 'productUnits': 'oz', 'productCost': '$3.60', 'id': 3
-    },
-    {
-        'productTitle': 'Honey Bunches of Oats Cereal', 'productPic': '../images/milk.svg', 'productQuantity': 1,
-        'productAmount': '14.5', 'productUnits': 'oz', 'productCost': '$6.99', 'id': 4
-    },
+
+
+shoppingHistoryProductsLarry = [
+  {
+      'productTitle': 'Milk', 'productPic': '../images/milk.svg', 'productQuantity': 2,
+      'productAmount': '2', 'productUnits': 'qts', 'productCost': '$5.00', 'id': 0
+  },
+  {
+      'productTitle': 'Danone Cheese', 'productPic': '../images/milk.svg', 'productQuantity': 1,
+      'productAmount': '12', 'productUnits': 'oz', 'productCost': '$7.00', 'id': 1
+  },
+  {
+      'productTitle': 'Lindt Assorted Truffles', 'productPic': '../images/milk.svg', 'productQuantity': 1,
+      'productAmount': '16', 'productUnits': 'oz', 'productCost': '$7.50', 'id': 2
+  },
+  {
+      'productTitle': 'Hollandia Butter', 'productPic': '../images/milk.svg', 'productQuantity': 3,
+      'productAmount': '10', 'productUnits': 'oz', 'productCost': '$3.60', 'id': 3
+  },
+  {
+      'productTitle': 'Honey Bunches of Oats Cereal', 'productPic': '../images/milk.svg', 'productQuantity': 1,
+      'productAmount': '14.5', 'productUnits': 'oz', 'productCost': '$6.99', 'id': 4
+  }
 ];
+
+shoppingHistoryProductsLucy = [
+  {
+      'productTitle': 'Lucys Favorite Butter', 'productPic': '../images/milk.svg', 'productQuantity': 2,
+      'productAmount': '2', 'productUnits': 'qts', 'productCost': '$5.00', 'id': 0
+  },
+  {
+      'productTitle': 'Danone Cheese', 'productPic': '../images/milk.svg', 'productQuantity': 1,
+      'productAmount': '12', 'productUnits': 'oz', 'productCost': '$7.00', 'id': 1
+  },
+  {
+      'productTitle': 'Lindt Assorted Truffles', 'productPic': '../images/milk.svg', 'productQuantity': 1,
+      'productAmount': '16', 'productUnits': 'oz', 'productCost': '$7.50', 'id': 2
+  },
+  {
+      'productTitle': 'Hollandia Butter', 'productPic': '../images/milk.svg', 'productQuantity': 3,
+      'productAmount': '10', 'productUnits': 'oz', 'productCost': '$3.60', 'id': 3
+  },
+  {
+      'productTitle': 'Honey Bunches of Oats Cereal', 'productPic': '../images/milk.svg', 'productQuantity': 1,
+      'productAmount': '14.5', 'productUnits': 'oz', 'productCost': '$6.99', 'id': 4
+  }
+];
+
+let shoppingHistoryProducts = [shoppingHistoryProductsLarry,shoppingHistoryProductsLucy];
 
 // This is the complete database of all the products in the store
 let storeProducts = [
